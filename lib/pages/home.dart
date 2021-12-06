772d8de0f523956a96db81cd521ad32c9e5eeee8
Chatter-Boo <psakhil211@gmail.com>
2021-12-06 19:54:41 +0530

    added list

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/items.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+import 'package:flutter_catalog/widgets/item_widget.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
@@ -18,6 +20,16 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
       ),
       drawer: MyDrawer(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+            itemCount: CatalogModels.items.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(
+                item: CatalogModels.items[index],
+              );
+            }),
+      ),
     );
   }
 }
